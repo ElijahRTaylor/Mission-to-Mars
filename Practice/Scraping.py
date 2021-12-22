3c70526681420e7a6c22fc368f26851a85e60c5a
Elijah Taylor <elijahtaylor@MacBook-Pro-2.lan>
2021-12-22 01:38:16 -0500

    Renamed files

--- Practice/Scraping.py
+++ Practice/Scraping.py
@@ -8,14 +8,30 @@ from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import datetime as dt
 
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
 def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
 
+    news_title, news_paragraph = mars_news(browser)
+
     # Run all scraping functions and store results in a dictionary
-    data = {}
-    #def mars_news(browser):
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now(),
+        "hemispheres":mars_hemispheres(browser)
+        }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
+
+def mars_news(browser):
 
     # Visit the mars nasa news site
     url = 'https://redplanetscience.com'
@@ -46,14 +62,11 @@ def scrape_all():
     except AttributeError:
         return None, None
     
-    data["news_title"] = news_title
-    data["featured_image"] =news_p
-    
+    return news_title, news_p
 
-    
-    #return news_title, news_p
-
-    # ### Featured Images    # Visit URL
+    # ### Featured Images
+def featured_image(browser):
+    # Visit URL
     url = 'https://spaceimages-mars.com'
     browser.visit(url)
 
@@ -74,12 +87,11 @@ def scrape_all():
     # Use the base URL to create an absolute URL
     img_url = f'https://spaceimages-mars.com/{img_url_rel}'
     
-
-    data["featured_image"]=img_url
+    return img_url
 
 
     #Mars Facts
-
+def mars_facts():
     try:
     #Convert first table on website into Pandas DataFrame
         df = pd.read_html('https://galaxyfacts-mars.com')[0]
@@ -90,47 +102,33 @@ def scrape_all():
     df.set_index('description', inplace=True)
     
     # Convert dataframe to html
-    data["facts"] = df.to_html(classes="table table-striped")
+    return df.to_html(classes="table table-striped")
 
+def mars_hemispheres():
     url = 'https://marshemispheres.com/'
     browser.visit(url)
-
-    # 2. Create a list to hold the images and titles.
     hemisphere_image_urls = []
 
-# 3. Write code to retrieve the image urls and titles for each hemisphere.
     for x in range(4):
-    # Browse through each article
         browser.links.find_by_partial_text('Enhanced')[x].click()
-    
         html = browser.html
         html_soup = soup(html,'html.parser')
-    
         title = html_soup.find('h2', class_='title').text
         img_url = html_soup.find('img', class_='wide-image').get('src')
-    
         print(img_url)
         print(title)
-    
-
         mars_hemispheres = {}
         mars_hemispheres['img_url'] = f'https://marshemispheres.com/{img_url}'
         mars_hemispheres['title'] = title
         hemisphere_image_urls.append(mars_hemispheres)
-
-         # Browse back to repeat
         browser.back()
     
-    
-    data["hemispheres"]=hemisphere_image_urls
-
-   
-
-    #Quit browser
-    browser.quit()  
-    return data
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
-# If running as script, print scraped data
+    # If running as script, print scraped data
     print(scrape_all())
+
+#Quit browser
+browser.quit()
--- Scraping.py
+++ Scraping.py
@@ -8,30 +8,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import datetime as dt
 
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
 def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
 
-    news_title, news_paragraph = mars_news(browser)
-
     # Run all scraping functions and store results in a dictionary
-    data = {
-        "news_title": news_title,
-        "news_paragraph": news_paragraph,
-        "featured_image": featured_image(browser),
-        "facts": mars_facts(),
-        "last_modified": dt.datetime.now(),
-        "hemispheres":mars_hemispheres(browser)
-        }
-
-    # Stop webdriver and return data
-    browser.quit()
-    return data
-
-def mars_news(browser):
+    data = {}
+    #def mars_news(browser):
 
     # Visit the mars nasa news site
     url = 'https://redplanetscience.com'
@@ -62,11 +46,14 @@ def mars_news(browser):
     except AttributeError:
         return None, None
     
-    return news_title, news_p
+    data["news_title"] = news_title
+    data["featured_image"] =news_p
+    
 
-    # ### Featured Images
-def featured_image(browser):
-    # Visit URL
+    
+    #return news_title, news_p
+
+    # ### Featured Images    # Visit URL
     url = 'https://spaceimages-mars.com'
     browser.visit(url)
 
@@ -87,11 +74,12 @@ def featured_image(browser):
     # Use the base URL to create an absolute URL
     img_url = f'https://spaceimages-mars.com/{img_url_rel}'
     
-    return img_url
+
+    data["featured_image"]=img_url
 
 
     #Mars Facts
-def mars_facts():
+
     try:
     #Convert first table on website into Pandas DataFrame
         df = pd.read_html('https://galaxyfacts-mars.com')[0]
@@ -102,33 +90,47 @@ def mars_facts():
     df.set_index('description', inplace=True)
     
     # Convert dataframe to html
-    return df.to_html(classes="table table-striped")
+    data["facts"] = df.to_html(classes="table table-striped")
 
-def mars_hemispheres():
     url = 'https://marshemispheres.com/'
     browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
     hemisphere_image_urls = []
 
+# 3. Write code to retrieve the image urls and titles for each hemisphere.
     for x in range(4):
+    # Browse through each article
         browser.links.find_by_partial_text('Enhanced')[x].click()
+    
         html = browser.html
         html_soup = soup(html,'html.parser')
+    
         title = html_soup.find('h2', class_='title').text
         img_url = html_soup.find('img', class_='wide-image').get('src')
+    
         print(img_url)
         print(title)
+    
+
         mars_hemispheres = {}
         mars_hemispheres['img_url'] = f'https://marshemispheres.com/{img_url}'
         mars_hemispheres['title'] = title
         hemisphere_image_urls.append(mars_hemispheres)
+
+         # Browse back to repeat
         browser.back()
     
-    return hemisphere_image_urls
+    
+    data["hemispheres"]=hemisphere_image_urls
+
+   
+
+    #Quit browser
+    browser.quit()  
+    return data
 
 if __name__ == "__main__":
 
-    # If running as script, print scraped data
+# If running as script, print scraped data
     print(scrape_all())
-
-#Quit browser
-browser.quit()
